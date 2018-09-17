77ea9960c7996bdd1d4ae54a9a30df32f8857c72
Anthony Dresser <andresse@microsoft.com>
2018-09-17 12:23:55 -0700

    working on input

--- src/sql/parts/query/editor/queryEditor.ts
+++ src/sql/parts/query/editor/queryEditor.ts
@@ -5,10 +5,15 @@
 
 import 'vs/css!sql/parts/query/editor/media/queryEditor';
 
+import { QueryInput } from 'sql/parts/query/common/queryInput';
+
 import { BaseEditor } from 'vs/workbench/browser/parts/editor/baseEditor';
 import { Dimension } from 'vs/base/browser/dom';
 import { SplitView } from 'vs/base/browser/ui/splitview/splitview';
 import * as DOM from 'vs/base/browser/dom';
+import { EditorOptions } from 'vs/workbench/common/editor';
+import { CancellationToken } from 'vs/base/common/cancellation';
+import { TPromise } from 'vs/base/common/winjs.base';
 
 export class QueryEditor extends BaseEditor {
 
@@ -20,5 +25,18 @@ export class QueryEditor extends BaseEditor {
 
 	public layout(dimension: Dimension): void {
 		throw new Error('Method not implemented.');
+	}
+
+	public get input(): QueryInput {
+		return this._input;
+	}
+
+	public setInput(input: QueryInput, options: EditorOptions, token: CancellationToken): Thenable<void> {
+		const oldInput = this.input;
+		super.setInput(input, options, token);
+
+		if (oldInput && oldInput.matches(this.input)) {
+			return TPromise.as(undefined);
+		}
 	}
 }
